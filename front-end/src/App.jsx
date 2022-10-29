e2ece05b50f556fd18f4ec8fcdc6f9365d6d1cfe
Matheus <matheuslnmoura@gmail.com>
2022-10-29 00:19:50 -0300

    Feat: Check if runner is working

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
   return (
     <CepInfoContext.Provider value={{CEP_PATTERN, cepValue, setCepValue, cepInfo, setCepInfo}}>
       <main className='app-wrapper'>
-        <h1>Bem vindo(a)!</h1>
+        <h1>Runner is up and running(hehehe) </h1>
         <Form />
         <CepInfo />
         <p>{cepInfo.message}</p>
